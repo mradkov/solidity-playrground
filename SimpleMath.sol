db163cd01b9e5e86b1faf699e6908363d93bc2c5
Milen Radkov <milenradkov@me.com>
2018-02-08 17:43:36 +0200

    Fixed simplemath homework
    Take your math contract and modify it so:
    The math functions take only one argument, number 2.
    Number 1 is kept as a state variable with initial value 0
    Each math function executes its operation with number 1 and 2 and stores the result in number 1
    Create a function to get the state (number 1)
    Create a function that resets the state to zero
    Test it and play with it.
    Perhaps add more complex logic?

--- SimpleMath.sol
+++ SimpleMath.sol
@@ -2,30 +2,41 @@ pragma solidity ^0.4.19;
 
 contract SimpleMath {
     
-    function add(int256 a, int256 b) public returns (int256) {
-        int256 c = a + b;
+    uint256 public a = 0;
+    
+    function add(uint256 b) public {
+        uint256 c = a + b;
         assert(c >= a);
-        return c;
+        a = c;
     }
     
-    function substract(int256 a, int256 b) public returns (int256) {
+    function substract(uint256 b) public {
         assert(b <= a);
-        return a - b;
+        a -= b;
     }
-        
-    function multiply(int256 a, int256 b) public returns (int256) {
-        int256 c = a * b;
+    
+    function multiply(uint256 b) public {
+        uint256 c = a * b;
         assert(a == 0 || c / a == b);
-        return c;
+        a = c;
     }
         
-    function divide(int256 a, int256 b) public returns (int256) {
-        int256 c = a / b;
-        return c;
+    function divide(uint256 b) public {
+        uint256 c = a / b;
+        a = c;
     }
             
-    function pow(uint256 a, uint256 b) public returns (uint256) {
+    function pow(uint256 b) public {
         uint256 result = a ** b;
-        return (result);
+        a = result;
     }
+    
+    function numberOneState() public returns (uint256) {
+        return a;
+    }
+    
+    function numberReset() public {
+        a = 0;
+    }
+
 }
