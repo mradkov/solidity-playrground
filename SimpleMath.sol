cf0c765469208dd2f8f05748aa2be0c3a89479fb
Milen Radkov <milenradkov@me.com>
2018-02-08 10:32:00 +0200

    Simple math + init tokensale
    Simple math + init tokensale

---
+++ SimpleMath.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.19;
+
+contract SimpleMath {
+    
+    function add(int256 a, int256 b) public returns (int256) {
+        int256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+    
+    function substract(int256 a, int256 b) public returns (int256) {
+        assert(b <= a);
+        return a - b;
+    }
+    
+        
+    function multiply(int256 a, int256 b) public returns (int256) {
+        int256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+        
+    function divide(int256 a, int256 b) public returns (int256) {
+        int256 c = a / b;
+        return c;
+    }
+            
+    function pow(uint256 a, uint256 b) public returns (uint256) {
+        uint256 result = a ** b;
+        return (result);
+    }
+}
